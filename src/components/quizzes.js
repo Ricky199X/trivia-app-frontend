72b2dfc0ab0d9a69dbdc988c75649066f08b272f
github username <github email address>
2019-12-09 05:38:56 -0500

    begins render method

--- src/components/quizzes.js
+++ src/components/quizzes.js
@@ -8,7 +8,20 @@ class Quizzes {
 
    fetchAndLoadQuizzes() {
       this.adapter.getQuizzes().then(quizzes => {
-         console.log(quizzes)
+         return quizzes
       })
+      .then(() => {
+         this.render()
+      })
+   }
+
+   // render quizzes to the DOM - call after we get all the quizzes
+
+   render() {
+      console.log('rendering!')
+      // needs to append to main
+      const main = document.querySelector('main')
+      // const quizMenu = document.getElementById('trivia-container')
+      // quizMenu.innerHTML - "testing the div"
    }
 }
